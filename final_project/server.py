e115b94b26ddcf9379ae0a7b2aaebc34d0c33484
MarkAI7 <can2mxf@hotmail.com>
2023-07-31 19:11:15 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from translator import english_to_french, french_to_english
+from .translator import english_to_french, french_to_english
 
 app = Flask("Web Translator")
 

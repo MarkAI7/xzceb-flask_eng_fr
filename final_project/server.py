3e816cd00cbced50546171bd0ad8de196dd8198b
MarkAI7 <can2mxf@hotmail.com>
2023-07-31 21:45:37 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,7 @@
-from flask import jsonify
+from flask import Flask, render_template, request, jsonify
+from machinetranslation.translator import english_to_french, french_to_english
+
+app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
@@ -11,3 +14,10 @@ def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     translated_text = french_to_english(textToTranslate)
     return jsonify({"translatedText": translated_text})
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
